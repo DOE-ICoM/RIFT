a7020f502ea8db3acfe808f6e8d47e6a24799e86
Vinay Amatya <vinay.amatya@pnnl.gov>
2023-01-19 15:21:47 -0800

    Add CMake build option for RIFT. Add helper code

--- io.h
+++ io.h
@@ -11,6 +11,27 @@ typedef struct Grid {
 	double cellsize;
 	double nodata;
 	double* data;
+	int rank;
+	int pcount;
+	Grid(const int cols, const int rows, const double x_lower_left,
+		 const double y_lower_left, const double cellsize_value = 0.0, 
+		 const double nodata_value= -9999.0)
+		 :num_columns(cols),num_rows(rows),xll(x_lower_left), yll(y_lower_left),
+		 cellsize(cellsize_value), nodata(nodata_value)
+	{}
+	void InitMPI(const int size, const int rank_p) { pcount = size; rank = rank_p;}
+	
+	void Fill(const double value)
+	{
+		data = (double*)malloc(sizeof(double)*num_columns*num_rows);
+		for(int i = 0; i < num_rows; i++)
+			for(int j=0; j<num_columns; j++)
+				data[i*num_columns + j] = 0;
+	}
+
+	double* get_data() {return data;}
+	int get_num_rows() {return num_rows;}
+	int get_num_columns() {return num_columns;}
 } Grid;
 
 Grid *ReadGrid  (std::string filename);
