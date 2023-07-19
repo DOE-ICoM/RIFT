fecbf6567749f3131f871c239bc2edc389d6da50
William Perkins <william.perkins@pnnl.gov>
2023-07-19 08:50:48 -0700

    Remove non-existent variable declarations

--- constants.h
+++ constants.h
@@ -8,22 +8,7 @@
 const double  h_epsilon = 1.19209e-07f;
 const double pi        = 3.14159265358979324;
 
-extern int   h_nx;    // abscissa grid dimension
-extern int   h_ny;    // ordinate grid dimension
-extern double h_dx;    // abscissa spatial resolution (m)
-extern double h_dy;    // ordinate spatial resolution (m)
-
-extern int    b_nx;
-extern int    b_ny;
-extern double b_xll;
-extern double b_yll;
-
 extern int GridSize;
-
-extern double h_xll;
-extern double h_yll;
-extern double cellsize_original;
-extern double cellsize;
 
 extern size_t pitch, pitchBX, pitchBY;
 
