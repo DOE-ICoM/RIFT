4da13ca8a64a241d210c7629c54c2259c61d4568
William A Perkins <william.perkins@pnnl.gov>
2024-09-24 09:06:17 -0700

    Add some command line options so all GridSeries can be excercised

--- GridSeriesTest.cpp
+++ GridSeriesTest.cpp
@@ -6,11 +6,15 @@
 // -------------------------------------------------------------
 
 #include <iostream>
+#include <libgen.h>
+#include <unistd.h>
 
 #include "constants.h"
 #include "io.h"
 #include "grid.h"
 #include "GridSeries.cuh"
+
+
 
 
 // -------------------------------------------------------------
@@ -19,37 +23,82 @@
 int
 main(int argc, char **argv)
 {
-  std::string basename;
-  std::string g0name;
+  
+  std::string progname(basename(argv[0]));
+  std::string usage(progname);
+  usage += " [-i] [-H] [-T tmax] [-d i_dt] [-s o_dt] dem basename";
 
+  bool dohyetograph(false);
+  bool dointerp(false);
+
+  int opt;
+  double instep(600), outstep(instep), tmax(86400);
+
+  while ((opt = getopt(argc, argv, "iHT:d:s:")) != -1) {
+    switch (opt) {
+    case 'H':
+      dohyetograph = true;
+      break;
+    case 'i':
+      dointerp = true;
+      break;
+    case 'T':
+      tmax = atof(optarg);
+      break;
+    case 'd':
+      instep = atof(optarg);
+      break;
+    case 's':
+      outstep = atof(optarg);
+      break;
+    default: /* '?' */
+      std::cerr << "error: unknown option '" << opt << "'" << std::endl;
+      std::cerr << "Usage: " << usage << std::endl;
+      exit(3);
+    }
+  }
+
+  if (argc - optind < 2) {
+    std::cerr << "Usage: " << usage << std::endl;
+    exit(3);
+  }
+
+  std::string g0name(argv[optind]);
+  std::string basename(argv[optind+1]);
   GridConfig gc;
   double *b;
 
   cudaSetDevice(0);
 
-  if (argc < 3) {
-  }
-
-  g0name = argv[1];
-  basename = argv[2];
-
+  
   InitBathymetry(b, g0name, gc);
 
   SetDeviceConstants(gc.h_nx, gc.h_ny, gc.h_dx, gc.h_dy, 1.0, gc.nodata);
 
-  double tmax = 86400;
-  double dt = 600;
-  std::unique_ptr<GridSeries>
-    g(new 
-#ifdef INTERPOLATE
-      InterpolatedGridSeries
-#else
-      GridSeries
-#endif
-      (basename, 1.0, dt, tmax, gc));
+  std::unique_ptr<GridSeries> g;
+  std::string msg("Exercising class "); 
+  if (dohyetograph) {
+    if (dointerp) {
+      msg += "Interpolated";
+      g.reset(new InterpolatedHyetographGridSeries(basename, instep, tmax, gc));
+    } else {
+      g.reset(new HyetographGridSeries(basename, instep, tmax, gc));
+    }
+    msg += "HyetographGridSeries";
+  } else {
+    if (dointerp) {
+      msg += "Interpolated";
+      g.reset(new InterpolatedGridSeries(basename, 1.0, instep, tmax, gc));
+    } else {
+      g.reset(new GridSeries(basename, 1.0, instep, tmax, gc));
+    }
+    msg += "GridSeries";
+  }
+  std::cout << msg << std::endl;
+  
   g->allow_no_data(true);
 
-  for (double t = 0; t <= tmax; t += dt) {
+  for (double t = 0; t <= tmax; t += outstep) {
     g->update(t);
     std::cout << "Time = " << t << ", "
               << "Sum = " << g->sum()
